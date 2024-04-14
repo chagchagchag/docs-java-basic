5de822721d7ebc9011ad57662bebe83f7796b12d
chagchagchag <chagchagchag.dev@gmail.com>
2024-04-14 13:43:19 +0900

    first commit

---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Docs Java Basic</span>,
+  project: {
+    link: 'https://github.com/chagchagchag/docs-java-basic',
+  },
+  docsRepositoryBase: 'https://github.com/chagchagchag/docs-java-basic',
+  footer: {
+    text: 'Nextra Docs Template',
+  },
+}
+
+export default config
